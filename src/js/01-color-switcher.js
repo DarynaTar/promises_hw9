f507fe6cb85be25142f58294d933c1ed374e8f14
DarynaTar <119743833+DarynaTar@users.noreply.github.com>
2023-05-14 08:53:29 +0300

    change bg color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]')
+const stopBtn = document.querySelector('button[data-stop]')
+
+
+startBtn.addEventListener('click', ()=>{
+
+    timerID = setInterval(()=>{
+    startBtn.disabled = true
+    stopBtn.disabled = false
+        document.body.style.backgroundColor = getRandomHexColor()
+    }, 1000)
+})
+
+stopBtn.addEventListener('click', ()=>{
+    startBtn.disabled = false
+    stopBtn.disabled = true
+    clearInterval(timerID)
+})
