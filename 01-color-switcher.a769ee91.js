b364caa45ba2e68a8b195fd976ec1b21c18d8137
DarynaTar <119743833+DarynaTar@users.noreply.github.com>
2023-05-18 05:05:11 +0000

    Deploying to gh-pages from @ DarynaTar/promises_hw9@a8fe827240f69a4a6e9e6cdf0687e8382e68fa7c 🚀

---
+++ 01-color-switcher.a769ee91.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(()=>{timerID=setInterval((()=>{t.disabled=!0,e.disabled=!1,document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(()=>{t.disabled=!1,e.disabled=!0,clearInterval(timerID)}));
+//# sourceMappingURL=01-color-switcher.a769ee91.js.map
